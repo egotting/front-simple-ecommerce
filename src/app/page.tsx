24890fbd004ab859288f7dd3694e419be0c3124e
egotting <henricodss@gmail.com>
2025-05-19 18:16:11 -0300

    refactor: Colocando o header

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,13 @@
 import styles from "./page.module.css";
+import {Header} from "@/Components/Header";
 
 export default function Home() {
     return (
-        <div className={styles.page}>
+        <div>
+            <Header/>
+            <main className={styles.main}>
+                <h1>teste</h1>
+            </main>
         </div>
     );
 }
