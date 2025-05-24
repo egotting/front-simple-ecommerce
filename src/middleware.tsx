b0295ed448216c71d013f8ea9ad639c202a44dde
egotting <henricodss@gmail.com>
2025-05-23 21:44:34 -0300

    .

--- src/middleware.tsx
+++ src/middleware.tsx
@@ -1,46 +1,44 @@
 import {NextRequest, NextResponse} from "next/server";
 
 const publicRoutes = [
-    {path: '/sign-in', whenAuthenticated: 'redirect'},
-    {path: '/sign-up', whenAuthenticated: 'redirect'},
-    {path: '/profile', whenAuthenticated: 'redirect'},
-    {path: '/my-cart', whenAuthenticated: 'next'},
-    {path: '/', whenAuthenticated: 'next'},
+    {path: "/sign-in", whenAuthenticated: "redirect"},
+    {path: "/sign-up", whenAuthenticated: "redirect"},
+    {path: "/profile", whenAuthenticated: "redirect"},
+    {path: "/my-cart", whenAuthenticated: "next"},
+    {path: "/", whenAuthenticated: "next"},
     // {path: '/dashboard', whenAuthenticated: 'next'}
 ] as const;
 
-const REDIRECT_WHEN_NOT_AUTHENTICATED = '/sign-in';
+const REDIRECT_WHEN_NOT_AUTHENTICATED = "/sign-in";
 
 export function middleware(req: NextRequest) {
     const path = req.nextUrl.pathname;
-    const publicRoute = publicRoutes.find(
-        route => route.path === path)
-    const getToken = req.cookies.get('token')?.value;
-
-
-    if (publicRoute === null || publicRoute === undefined) {
+    const showFlyout = publicRoutes.find((route) => route.path === path);
+    const getToken = req.cookies.get("token")?.value;
+    if (showFlyout === null || showFlyout === undefined) {
         throw new Error("Not found public route");
     }
-    if (!getToken && !publicRoute) {
+    if (!getToken && !showFlyout) {
         const redirectUrl = req.nextUrl.clone();
         redirectUrl.pathname = REDIRECT_WHEN_NOT_AUTHENTICATED;
         return NextResponse.redirect(redirectUrl);
     }
-    if (!getToken && publicRoute) {
+    if (!getToken && showFlyout) {
         return NextResponse.next();
     }
 
-    if (getToken && publicRoutes && publicRoute?.whenAuthenticated === 'redirect') {
-
-        const redirectUrl = req.nextUrl.clone()
-        redirectUrl.pathname = '/';
+    if (
+        getToken &&
+        publicRoutes &&
+        showFlyout?.whenAuthenticated === "redirect"
+    ) {
+        const redirectUrl = req.nextUrl.clone();
+        redirectUrl.pathname = "/";
         return NextResponse.redirect(redirectUrl);
     }
 
-
     return NextResponse.next();
 }
-
 
 export const config = {
     matcher: [
@@ -51,6 +49,6 @@ export const config = {
          * - _next/image (image optimization files)
          * - favicon.ico, sitemap.xml, robots.txt (metadata files)
          */
-        '/((?!api|_next/static|_next/image|favicon.ico|sitemap.xml|robots.txt).*)',
+        "/((?!api|_next/static|_next/image|favicon.ico|sitemap.xml|robots.txt).*)",
     ],
-}
+};
